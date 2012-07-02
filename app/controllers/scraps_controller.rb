7b2fc7ca8dae7405f09ea10dda17bad39d98db48
William Ross <will@spanner.org>
2012-07-02 10:55:53 +0100

    scraps controller

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,2 +1,82 @@
 class ScrapsController < ApplicationController
+
+  before_filter :authenticate_user!
+  before_filter :find_scraps, :only => [:index]
+  before_filter :get_scrap, :only => [:show, :edit, :update, :destroy]
+  before_filter :build_scrap, :only => [:new, :create]
+  before_filter :update_scrap, :only => [:create, :update]
+
+  def index
+    respond_with(@scraps) do |format|
+      format.js { render :partial => 'search_results' }
+    end
+  end
+
+  def show
+    respond_with(@scrap)
+  end
+
+  def edit
+    respond_with(@scrap)
+  end
+
+  def update
+    respond_with(@scrap)
+  end
+
+protected
+
+  def find_scraps
+    sorts = {
+      :name => 'ASC',
+      :date => 'DESC',
+      :updated_at => 'DESC',
+      :created_at => 'DESC',
+      :published_at => 'DESC'
+    }
+    @by = params[:by] || 'date'
+    @by = 'date' unless sorts[@by.to_sym]
+    @order = params[:order] || sorts[@by.to_sym] || 'ASC'
+    
+    @scraps = Scrap.order("scraps.#{@by} #{@order}")
+    @tags = [params[:tag]].flatten.map{|t| Tag.find_by_id(t) }.compact
+    @fragments = []
+
+    unless params[:q].blank?
+      [params[:q]].flatten.reject(&:blank?).each do |fragment|
+        if institution = Institution.find_by_name(fragment)
+          @institutions.push(institution)
+        elsif tag = Tag.find_by_name(fragment)
+          @tags.push(tag)
+        else
+          @fragments += fragment.split(/\s+/)
+        end
+      end
+    end
+
+    @rejected_fragments = @fragments.select {|frag| STOPWORDS.include?(frag) }
+    @fragments = (@fragments - @rejected_fragments).compact.uniq
+    @scraps = @scraps.tagged_with_all_of(@tags) if @tags.any?
+    @fragments.each { |frag| @scraps = @scraps.matching(frag) }
+
+    # used in facet display
+    @criteria = (@tags + @institutions).sort_by(&:name)
+
+    @show = params[:show] || 20
+    @page = params[:page] || 1
+    @scraps = @scraps.with_associations.page(@page).per(@show) unless @show == 'all'
+  end
+
+  def get_scrap
+    @scrap = scrap.find(params[:id])
+  end
+
+  def build_scrap
+    @scrap = scrap.new
+  end
+  
+  def update_scrap
+    @scrap.update_attributes(params[:scrap])
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 will = User.create(:email => 'will@spanner.org', :forename => "Will", :surname => "Ross", :password => "foobar", :password_confirmation => "foobar")
 mike = User.create(:email => 'mike@spanner.org', :forename => "Mike", :surname => "McIver", :password => "foobar", :password_confirmation => "foobar")
 
-text_scrap = Scrap.create(:name => 'test scrap', :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+text_scrap = Scrap.create(:name => 'text scrap', :body => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
 word_scrap = Scrap.create(:name => 'word scrap', :body => "Merciful disquiet")
 
 upload = Upload.create(:file => File.new(Rails.root + 'app/assets/images/hunters.jpg'))
