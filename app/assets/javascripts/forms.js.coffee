83d542ac46afddf90a9bfeaf87371ff6c80cddfb
mike1104 <mike@spanner.org>
2012-07-03 15:35:14 +0100

    big graph captions

--- app/assets/javascripts/forms.js.coffee
+++ app/assets/javascripts/forms.js.coffee
@@ -306,5 +306,5 @@ $ ->
   $('.error_message').validation_error()
   $('.toggle').toggle()
   $('.tab').tab()
-  $('textarea.body').self_sizes()
+  # $('textarea.body').self_sizes()
   $('.dropbox').uploader()
--- app/models/scrap.rb
+++ app/models/scrap.rb
@@ -21,7 +21,7 @@ class Scrap < ActiveRecord::Base
     count = reactions.count
     json = {
       :size => count,
-      :scrap_id => name
+      :name => name
     }
     scores.each do |score|
       unless json[score.scale.name]
